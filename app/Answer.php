72d7c16ecbc1a6a79aa5f3cdab6779e145cbac34
Shasika <shasikamadushan555@gmail.com>
2020-04-19 12:51:52 +0530

    Update best_answer_id on questions table when an answer removed using Eloquent event

--- app/Answer.php
+++ app/Answer.php
@@ -29,7 +29,12 @@ class Answer extends Model
         });
 
         static::deleted(function($answer){
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if($question->best_answer_id == $answer->id){
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
         
     }
@@ -37,5 +42,9 @@ class Answer extends Model
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
+    }
+    public function getStatusAttibute()
+    {
+        return $this->id == $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
